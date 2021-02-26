22849df940ff6fe475297627a06562e112a2151a
shoalam <shoalamdu@gmail.com>
2021-02-27 02:15:38 +0600

    docs updated

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const formInput = document.getElementById('form-control');
 const button = document.querySelector('.todobtn');
+const list = document.querySelector('.todolist');
 
+// button.addEventListener('click', function(){
+    
+//     const newList = document.createElement('li');
+//     newList.innerHTML = "Hello World";
+//     list.appendChild(newList);
+// })
+
+const newList = document.createElement('li');
+newList.innerHTML = formInput.value;
+list.appendChild(newList);
